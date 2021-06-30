cce2af2f131d58f008ff7c94e49be986c40db822
Almurat98 <almurat908@gmail.com>
2021-06-30 18:47:26 -0500

    new branch us100 work 1

--- src/test/java/UserStory10.java
+++ src/test/java/UserStory10.java
@@ -2,6 +2,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class UserStory10 {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("work 1 ");
     }
 }
